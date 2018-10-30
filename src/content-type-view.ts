48c84ca3719d34719722c9fd013d40fd5bd553dc
Alex Rempel <alexey.rempel@gmail.com>
2018-10-30 08:31:51 +0100

    simpler structure for dist, content type view

---
+++ src/content-type-view.ts
@@ -0,0 +1,47 @@
+function arrayToHtmlElement(data: any[]) {
+  return data.length > 0 ? `<ol start="0">${data.map(val => `<li>${toHtmlElement(val)}</li>`).join('')}</ol>` : '-';
+}
+
+function entriesToHtmlElement(data: any[]) {
+  return data.length > 0 ? `<ul>${data.map(([key, val]) => `<li>${key}: ${toHtmlElement(val)}</li>`).join('')}</ul>` : '-';
+}
+
+function toHtmlElement(data: any): string {
+  return data === undefined ? '<span style="color: red;">undefined</span>' :
+    data === null ? '<span style="color: red;">null</span>' :
+      typeof data !== 'object' ? `<label>${data.toString()}</label>` :
+        Array.isArray(data) ? arrayToHtmlElement(data) :
+          data instanceof Date ? data.toISOString() :
+            entriesToHtmlElement(Object.entries(data));
+}
+
+function toHtmlView(data: any) {
+  return `
+  <!DOCTYPE html>
+  <html>
+  <head>
+  <meta charset="UTF-8">
+  </head>
+  <body style="font-family:Roboto,\"Helvetica Neue\",sans-serif;">
+    ${toHtmlElement(data)}
+  </body>
+  </html>`
+}
+
+function toJsonView(data: any) {
+  return JSON.stringify(data, null, 2);
+}
+
+function toTextView(data: any) {
+  return data === undefined || data === null ? '' : data.toString();
+}
+
+export function toContentTypeView(contentType: string, data: any) {
+  let ret = <string>null;
+  switch (contentType) {
+    case 'application/json': ret = toJsonView(data); break;
+    case 'text/html': ret = toHtmlView(data); break;
+    default: ret = toTextView(data); break;
+  }
+  return ret;
+}
---
+++ src/hello.server.ts
@@ -0,0 +1,40 @@
+import { createServer } from 'http';
+import { parse } from 'url';
+import { toContentTypeView } from './content-type-view';
+
+const ARGV = process.argv
+  .filter(_ => _.startsWith('-par:'))
+  .map(_ => _.substr('-par:'.length))
+  .filter(_ => _.length);
+
+const GREETING = (ARGV.find(_ => _.startsWith('greeting=')) || 'greeting=Hi').split('=')[1];
+const PORT = +(ARGV.find(_ => _.startsWith('port=')) || 'port=8080').split('=')[1];
+
+const QUERY_USER = 'user';
+const QUERY_CONTENT_TYPE = 'contentType';
+
+const server = createServer((req, resp) => {
+  const query = parse(req.url || '');
+  const params = (query.query || '').split('&');
+  const paramUserValue = (params.find(_ => _.startsWith(QUERY_USER + '=')) || QUERY_USER + '=').split('=')[1];
+  const paramContentType = (params.find(_ => _.startsWith(QUERY_CONTENT_TYPE + '=')) || QUERY_CONTENT_TYPE + '=').split('=')[1];
+
+  const contentType = paramContentType || req.headers['content-type'] || 'text/plain';
+
+  resp.setHeader('access-control-allow-origin', '*');
+  resp.setHeader('content-type', contentType);
+  resp.statusCode = 200;
+  resp.write(
+    toContentTypeView(contentType, {
+      _argv: ARGV,
+      _contentType: contentType,
+      _greetingPrefix: GREETING,
+      _urlQueryParamsCurrent: params,
+      _urlQueryParamsPossible: [QUERY_CONTENT_TYPE, QUERY_USER],
+      greeting: GREETING + ' ' + paramUserValue,
+    }));
+  resp.end();
+});
+
+console.log(`greeting server starting on ${PORT}`);
+server.listen(PORT);
